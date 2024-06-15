faddad5fc8486bda989be8f0ffb1465b1aafa9c1
Antonio <tato_fos@hotmail.com>
2024-06-15 17:47:45 -0400

    21. Add Entity Framework feature

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace WebAPIAutores
 {
     public class Startup
@@ -13,6 +15,10 @@ namespace WebAPIAutores
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
