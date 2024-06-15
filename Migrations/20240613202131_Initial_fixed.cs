113d19eb273669b1a9e89ecf448369032e49bc6c
Antonio <tato_fos@hotmail.com>
2024-06-15 17:48:07 -0400

    Add Initial Author Migrations

---
+++ Migrations/20240613202131_Initial_fixed.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPIAutores.Migrations
+{
+    public partial class Initial_fixed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
