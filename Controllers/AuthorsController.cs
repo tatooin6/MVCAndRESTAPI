7d522e39f4fb3103f80a89bf71d6e64dac29bf57
Antonio <tato_fos@hotmail.com>
2024-06-15 18:45:26 -0400

    22. Add post and get author to controller

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -1,4 +1,6 @@
+using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebAPIAutores.Entities;
 
 namespace WebAPIAutores.Controllers
@@ -7,13 +9,25 @@ namespace WebAPIAutores.Controllers
     [Route("api/authors")]
     public class AuthorsController: ControllerBase
     {
-        [HttpGet]
-        public ActionResult<List<Author>> Get()
+        private readonly ApplicationDbContext context;
+        public AuthorsController(ApplicationDbContext context)
         {
-            return new List<Author>() {
-                new Author() { Id = 1, Name = "Tato" },
-                new Author() { Id = 2, Name = "Canela" }
-            };
-        }       
+            this.context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Author>>> Get()
+        {
+            return await context.Authors.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Author author)
+        {
+            context.Add(author);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
